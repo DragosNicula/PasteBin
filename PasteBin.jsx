87f5ee4f1097aa3ae2d002cad79d0fde59deb802
Nicula Dragos <110388376+DragosNicula@users.noreply.github.com>
2022-11-07 15:11:43 +0200

    Add files via upload

---
+++ PasteBin.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Register } from './Page/Register';
+import { Login } from './Page/Login';
+import { List } from './Page/List';
+import { Home } from './Page/Home';
+import { AddPost } from './Page/AddPost';
+import { Link, Routes, Route} from 'react-router-dom';
+import { getFirestore } from "firebase/firestore"; 
+
+export default function PasteBin() {
+    const [allPosts, setAllPosts] = useState([]);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const db = getFirestore();
+
+    return(
+            <div>
+                <Link to="/Home">Home</Link>
+                <a> </a>
+                <Link to="/AddPost">Add Post</Link>
+                <a> </a>
+                <Link to="/List">List</Link>
+                <a> </a>
+                <Link to="/Register">Register</Link>
+                <a> </a>
+                <Link to="/Login">Login</Link>
+                <Routes>
+                    <Route path="/" element={<Home db={db} allPosts={allPosts} userLoggedIn={userLoggedIn}/>} /> {/* When there is nothing after "/" it goes to home page*/}
+                    <Route path="/Home" element={<Home db={db} allPosts={allPosts} userLoggedIn={userLoggedIn} />} /> {/* Home Page Route*/}
+                    <Route path="/AddPost" element={<AddPost db={db} allPosts={allPosts} userLoggedIn={userLoggedIn}/>} /> {/* AddPost Page Route*/}
+                    <Route path="/List" element={<List db={db} userLoggedIn={userLoggedIn} allPosts={allPosts}/>} /> {/* AddMovie Page Route*/}
+                    <Route path="/Register" element={<Register setUserLoggedIn={setUserLoggedIn}/>} /> {/* Register Page Route*/}
+                    <Route path="/Login" element={<Login setUserLoggedIn={setUserLoggedIn}/>} />{/* Login Page Route*/}
+                </Routes>
+            </div>
+    )
+}
